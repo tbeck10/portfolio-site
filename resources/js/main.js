d3600e17ff4ec380bf9c8ef5b1c9474c62fe40b2
tbeck10 <trbeck10@gmail.com>
2018-02-08 15:39:31 -0500

    Add logo and javascript for smooth transitions

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
 
+  //Offset navbar when clicking anchor
+
+  $(".navbar a").on("click", function(event){
+    event.preventDefault();
+    var navHeight = $("nav").height();
+    var id = $(this).attr("href");
+    var originalScrollCoordinate = $(id).offset().top;
+    var newScrollCoordinate = originalScrollCoordinate - navHeight;
+    $("body, html").animate({
+      scrollTop : newScrollCoordinate
+    });
+  });
+
+  //Offset & animate jumbotron anchor
+
+  $(".jumbotron a").on("click", function(event){
+    event.preventDefault();
+    var navHeight = $("nav").height();
+    var id = $(this).attr("href");
+    var originalScrollCoordinate = $(id).offset().top;
+    var newScrollCoordinate = originalScrollCoordinate - navHeight;
+    $("body, html").animate({
+      scrollTop : newScrollCoordinate
+    });
+  });
+
 
 });
