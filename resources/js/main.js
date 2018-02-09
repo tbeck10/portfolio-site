81a773491b32d389caa44e05f42f81d7930b39e3
tbeck10 <trbeck10@gmail.com>
2018-02-09 12:54:57 -0500

    Add first subpage, paley institute

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,9 +3,20 @@ $(document).ready(function() {
   //Offset navbar when clicking anchor
 
   $(".navbar a").on("click", function(event){
-    event.preventDefault();
+    //event.preventDefault();
     var navHeight = $("nav").height();
     var id = $(this).attr("href");
+    var originalScrollCoordinate = $(id).offset().top;
+    var newScrollCoordinate = originalScrollCoordinate - navHeight;
+    $("body, html").animate({
+      scrollTop : newScrollCoordinate
+    });
+  });
+
+  $(".navbar a").on("click", function(event){
+    //event.preventDefault();
+    var navHeight = $("nav").height();
+    var id = $(index.html).attr("href");
     var originalScrollCoordinate = $(id).offset().top;
     var newScrollCoordinate = originalScrollCoordinate - navHeight;
     $("body, html").animate({
